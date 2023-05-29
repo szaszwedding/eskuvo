8180aeafff9b498a745d8385725e292df00a26e4
Zoltan Marton <martonzoltan4@gmail.com>
2023-05-29 13:05:29 +0300

    Init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,178 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<div id="app">
+		<div class="container">
+			<div class="row">
+				<div class="mt-4">
+					<div class="upload-section d-flex justify-content-center">
+						<input
+							type="file"
+							ref="fileInput"
+							accept="image/*, video/*"
+							style="display: none"
+							@change="handleFileInputChange"
+							multiple
+						/>
+						<button class="btn btn-primary me-3" @click="openFileInput">
+							Valaszd ki a kepeket
+						</button>
+						<button class="btn btn-primary" @click="uploadFiles">
+							Feltoltes
+						</button>
+					</div>
+				</div>
+			</div>
+			<div class="row mt-4">
+				<div class="col">
+					<div class="header-image-container">
+						<img
+							src="https://weddingdz.blob.core.windows.net/images/albom-borito_1685352002325.jpeg"
+							alt="Header Image"
+							class="img-fluid"
+						/>
+					</div>
+				</div>
+			</div>
+
+			<div class="row mt-4">
+				<div class="col">
+					<div class="grid-container">
+						<a
+							v-for="(image, index) in images"
+							:key="index"
+							:href="image"
+							class="grid-item"
+						>
+							<img :src="image" alt="Image" />
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { BlobServiceClient } from "@azure/storage-blob";
+import SimpleLightbox from "simplelightbox";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+	data() {
+		return {
+			images: [],
+			lightbox: null,
+			selectedFiles: [],
+			connectionSAS: process.env.VUE_APP_CONNECTION_SAS,
+			containerName: "images",
+		};
+	},
 
+	async mounted() {
+		await this.fetchImages();
+		this.lightbox = new SimpleLightbox(".grid-container a");
+	},
+	methods: {
+		async fetchImages() {
+			try {
+				const blobServiceClient = new BlobServiceClient(this.connectionSAS);
+				const containerClient = blobServiceClient.getContainerClient(
+					this.containerName
+				);
+
+				const blobItems = containerClient.listBlobsFlat({
+					prefix: "",
+					includeMetadata: true,
+				});
+
+				const sortedBlobs = [];
+				for await (const blob of blobItems) {
+					sortedBlobs.push(blob);
+				}
+
+				sortedBlobs.sort(
+					(a, b) =>
+						b.properties.createdOn.valueOf() - a.properties.createdOn.valueOf()
+				);
+
+				this.images = sortedBlobs.map(
+					(blob) => containerClient.getBlobClient(blob.name).url
+				);
+			} catch (error) {
+				console.error("Error retrieving images:", error);
+			}
+		},
+
+		openFileInput() {
+			this.$refs.fileInput.click();
+		},
+
+		handleFileInputChange(event) {
+			this.selectedFiles = Array.from(event.target.files);
+		},
+
+		async uploadFiles() {
+			try {
+				const files = this.selectedFiles;
+				const blobServiceClient = new BlobServiceClient(this.connectionSAS);
+				const containerClient = blobServiceClient.getContainerClient(
+					this.containerName
+				);
+
+				for (let i = 0; i < files.length; i++) {
+					const file = files[i];
+					const uniqueName = this.generateUniqueBlobName(file.name);
+					const blockBlobClient =
+						containerClient.getBlockBlobClient(uniqueName);
+					await blockBlobClient.uploadBrowserData(file);
+				}
+
+				console.log("Files uploaded successfully!");
+				await this.fetchImages();
+			} catch (error) {
+				console.error("Error uploading files:", error);
+			}
+		},
+
+		generateUniqueBlobName(originalName) {
+			const uniqueIdentifier = Date.now();
+			const fileName = originalName.substr(0, originalName.lastIndexOf("."));
+			const fileExtension = originalName.substr(originalName.lastIndexOf("."));
+
+			return `${fileName}_${uniqueIdentifier}${fileExtension}`;
+		},
+	},
+};
+</script>
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.grid-container {
+	display: grid;
+	grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+	grid-gap: 10px;
+}
+
+.grid-item {
+	display: block;
+	width: 100%;
+	height: auto;
+	max-width: 100%;
+	max-height: 100%;
+}
+
+img {
+	width: 100%;
+}
+
+input[type="file"]::file-selector-button {
+	margin-right: 20px;
+	border: none;
+	background: #084cdf;
+	padding: 10px 20px;
+	border-radius: 10px;
+	color: #fff;
+	cursor: pointer;
+	transition: background 0.2s ease-in-out;
+}
+
+input[type="file"]::file-selector-button:hover {
+	background: #0d45a5;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 createApp(App).mount('#app')
